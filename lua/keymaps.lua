3d49b5c63991ea92fbdb6efa407ce2f9139b6e60
domien <domien.meeus@zinderlabs.com>
2025-05-15 12:27:09 +0200

    Keymap: Primeagen keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -55,4 +55,11 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 
 vim.keymap.set('n', '<leader>bd', ':bdelete<CR>', { desc = '[B]uffer [D]elete' })
 
+--Primeagen Keymaps
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+vim.keymap.set('n', 'J', 'mzJ`z')
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('x', '<leader>p', '"_dP')
 -- vim: ts=2 sts=2 sw=2 et
